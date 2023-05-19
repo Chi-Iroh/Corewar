3076ec301f2463001b61a15d095fa74bb17c7614
Thomas Sayen <thomas.sayen@epitech.eu>
2023-05-19 16:03:06 +0200

    Added st mnemonic

--- corewar/mnemonics/st.c
+++ corewar/mnemonics/st.c
@@ -1,29 +1,28 @@
 /*
 ** EPITECH PROJECT, 2023
-** ld.c
+** st.c
 ** File description:
-** -> ld mnemonic
+** -> st mnemonic
 */
 
 #include "../../include/my_macros.h"
 #include "../../include/corewar/corewar.h"
 
-bool mnemonic_ld(vm_t *vm, vm_champion_t *champion, vm_mnemonic_args_t args)
+bool mnemonic_st(vm_t *vm, vm_champion_t *champion, vm_mnemonic_args_t args)
 {
     vm_register_t *register_address = NULL;
     vm_address_t indirect_arg_address = 0;
     uint8_t *memory_address = NULL;
 
     RETURN_VALUE_IF(!vm || !champion, false);
-    RETURN_VALUE_IF(!mnemonic_are_args_ok("ld", args), false);
-    register_address = &champion->registers[args.args[1]];
-    if (args.args_type[0] == PARAMETER_DIRECT) {
-        *register_address = args.args[0];
+    RETURN_VALUE_IF(!mnemonic_are_args_ok("st", args), false);
+    register_address = &champion->registers[args.args[0]];
+    if (args.args_type[1] == PARAMETER_REGISTER) {
+        *register_address = champion->registers[args.args[1]];
     } else {
         indirect_arg_address = (champion->pc + args.args[0]) % INDEX_MODULO;
         memory_address = &vm->memory[indirect_arg_address];
         my_memcpy(register_address, memory_address, REGISTER_SIZE);
     }
-    champion->carry = args.args[0] == 0 ? CARRY_ON : CARRY_OFF;
     return true;
 }
