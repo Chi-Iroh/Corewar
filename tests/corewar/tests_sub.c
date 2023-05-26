ce25c3aadd704a31b104ba46872d2dae8b6428d9
Thomas Sayen <thomas.sayen@epitech.eu>
2023-05-26 14:48:37 +0200

    Fixed SUB tests

--- tests/corewar/tests_sub.c
+++ tests/corewar/tests_sub.c
@@ -84,7 +84,7 @@ Test(mnemonic_sub, test_sub) {
         .load_address = 0,
         .number = 0,
         .pc = 0,
-        .registers = {},
+        .registers = { 139, 63 },
         .size = 0,
     };
     vm_t vm = {
@@ -102,7 +102,7 @@ Test(mnemonic_sub, test_sub) {
     printf("%s :\n", args.mnemonic);
     write_instruction(&vm, args, load_address, false);
     cr_assert(mnemonic_sub(&vm, &champion, args));
-    const vm_register_t expected = champion.registers[args.args[0] - 1] + champion.registers[args.args[1] - 1];
-    printf("\tExpected : %X / Got : %X\n", expected, champion.registers[0]);
+    const vm_register_t expected = champion.registers[args.args[0] - 1] - champion.registers[args.args[1] - 1];
+    printf("\tExpected : %X / Got : %X\n", expected, champion.registers[args.args[2] - 1]);
     cr_assert(champion.registers[args.args[2] - 1] == expected);
 }
