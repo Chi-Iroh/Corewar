95685eeb7680117b06b9ff559414ccd7bcc426fe
Thomas Sayen <thomas.sayen@epitech.eu>
2023-05-26 12:58:36 +0200

    Fixed macro in VM tests to use indexes

--- tests/corewar/tests.h
+++ tests/corewar/tests.h
@@ -9,7 +9,7 @@
 #include <memory.h>
 #include "../../include/corewar/corewar.h"
 
-#define VM_INDEX(x) ((x) < 0 ? 0x1'00'00 - (x) : (x))
+#define VM_INDEX(x) ((x) < 0 ? 0x1'00'00 - -(x) : (x))
 
 // mnemonic is not a pointer but a copy because this function is destructive
 void write_instruction(vm_t *vm, vm_mnemonic_t mnemonic, vm_address_t address, bool zero_init_all_memory);
