401e0bfecda3d9fbbbd0a5df2eefa08969094766
PERRIOLAT Nils <nils.perriolat@epitech.eu>
2023-05-28 08:31:10 -0400

    [FIX] : ldi's unit test

--- tests/corewar/tests_ldi.c
+++ tests/corewar/tests_ldi.c
@@ -94,19 +94,46 @@ Test(mnemonic_ldi, test_ldi) {
     };
     vm_mnemonic_t args = {
         .mnemonic = "ldi",
-        .args = { 1, 1, 2 },
-        .type = { PARAMETER_REGISTER, PARAMETER_DIRECT, PARAMETER_REGISTER },
+        .args = { 139, 4, 1 },
+        .type = { PARAMETER_DIRECT, PARAMETER_DIRECT, PARAMETER_REGISTER },
         .op = &OP_TAB(MNEMONIC_LDI)
     };
-    vm_address_t load_address = 0;
-    write_instruction(&vm, args, load_address, false);
-    cr_assert(mnemonic_ldi(&vm, &champion, args));
-    vm_register_t expected = 0;
     printf("%s :\n", args.mnemonic);
-    for (vm_address_t i = load_address; i < load_address + sizeof(expected); i++) {
+    write_instruction(&vm, args, 0, false);
+    cr_assert(mnemonic_ldi(&vm, &champion, args));
+    vm_register_t expected = args.args[0] + args.args[1];
+    printf("\tExpected : %X / Got : %X\n", expected, champion.registers[args.args[2] - 1]);
+    cr_assert(champion.registers[args.args[2] - 1] == expected);
+
+    args.type[0] = PARAMETER_INDIRECT;
+    args.args[0] = 31 - REGISTER_SIZE + 1;
+    write_instruction(&vm, args, 0, true);
+    vm.memory[31] = 139;
+    expected = 0;
+    cr_assert(mnemonic_ldi(&vm, &champion, args));
+    for (unsigned i = REGISTER_SIZE - 1; ; i--) {
         expected <<= 8;
-        expected |= vm.memory[i];
+        expected |= vm.memory[31 - i];
+        if (i == 0) {
+            break;
+        }
     }
-    printf("\tExpected : %X / Got : %X\n", expected, champion.registers[0]);
+    printf("\tExpected : %X / Got : %X\n", expected, champion.registers[args.args[2] - 1]);
+    cr_assert(champion.registers[args.args[2] - 1] == expected);
+
+    args.args[0] += INDEX_MODULO;
+    write_instruction(&vm, args, 0, true);
+    vm.memory[31] = 139;
+    expected = 0;
+    cr_assert(mnemonic_ldi(&vm, &champion, args));
+    for (unsigned i = REGISTER_SIZE - 1; ; i--) {
+        expected <<= 8;
+        expected |= vm.memory[31 - i];
+        if (i == 0) {
+            break;
+        }
+    }
+    cr_assert(mnemonic_ldi(&vm, &champion, args));
+    printf("\tExpected : %X / Got : %X\n", expected, champion.registers[args.args[2] - 1]);
     cr_assert(champion.registers[args.args[2] - 1] == expected);
 }
